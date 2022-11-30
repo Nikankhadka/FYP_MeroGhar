e1f3f6c788d73fb62e3ab53fc4fa27f446ff4252
nikan <np03cs4s210198@heraldcollege.edu.np>
2022-12-01 00:09:37 +0545

    modify boiler plate

--- meroghar/tailwind.config.js
+++ meroghar/tailwind.config.js
@@ -2,14 +2,14 @@
 module.exports = {
 //the purgr property is used to remove unused css from the final build
   purge: [
-    './pages/**/*.tsx',
+    './src/pages/**/*.tsx',
     // add more paths
-    './components/**/*.tsx',
+    './src/components/**/*.tsx',
 ],
 
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
