7a28344e3c0c42f5c9fc75e857dc273cbb736ce5
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-01-19 11:41:45 +0545

    updates the next boiler plate with next 13 setup

--- meroghar/tailwind.config.js
+++ meroghar/tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
 //the purgr property is used to remove unused css from the final build
   content: [
-    './src/pages/**/*.tsx',
+    './src/app/**/*.tsx',
     // add more paths
     './src/components/**/*.tsx',
 ],
 
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
