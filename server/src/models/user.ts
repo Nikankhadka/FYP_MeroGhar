d6d7dc5627d12c4ef6ea32117ad173aaf67db982
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-01-27 09:50:25 +0545

    updated the schema model for user so that it can be used for both admin and normal user

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -64,11 +64,14 @@ const userSchema=new Schema({
                 city:String,  
             },
             //multiple images of user, citizenship for manual verification
-            img:[{
-                //userId_img1/2
-                img_id:String,
-                img_url:String
-            }]
+            img:{
+                type:[{
+                    //userId_img1/2
+                    img_id:String,
+                    img_url:String
+                }],
+                default:undefined
+            }
 
         },
         
@@ -85,16 +88,28 @@ const userSchema=new Schema({
         //document id of refrenced product donot create new document in different collection
         wishList:{
             type:[{type:Schema.Types.ObjectId,ref:"property"}],
+            default:undefined
+        }
+        ,
            
-        },
+        
 
         //can be modified by admin to ban user for certain time or permanently
-        is_banned:{type:Number},
+        is_banned:{
+            strikes:Number,
+            banTime:Date
+        },
 
-        rented_property:[{type:Schema.Types.ObjectId,ref:"property"}],
+        rented_property:{
+            type:[{type:Schema.Types.ObjectId,ref:"property"}],
+            default:undefined
+        },
 
         //for recommendation colloborative information data will be rating and review 
-        recommendation:[String],
+        recommendation:{
+            type:[String],
+            default:undefined
+        },
 
 
         
