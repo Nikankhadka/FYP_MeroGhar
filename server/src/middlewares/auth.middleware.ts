6b6ec1bec822205c4e2e26cafad17f9573d0fb2c
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-01-29 13:01:14 +0545

    re applied roel verification

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -6,11 +6,7 @@ import { verifyAccessTokenS } from "../services/auth.service";
 //combined verify token+role in single middle ware 
 //takes in custom value of admin or not first verifies the token data then verifies role with passed role 
 //token verification works same for all just pass the role for rout if admin only true else false
-export const verifyaccessToken=async(is_Admin:boolean)=>{
-  
-
-    return async(req:Request,res:Response,next:NextFunction)=>{
-
+export const verifyaccessToken=async(req:Request,res:Response,next:NextFunction)=>{
         if(!req.cookies.accessToken) return res.status(401).json({success:false,message:"access token not found"});
         const {accessToken}=req.cookies;
 
@@ -18,9 +14,6 @@ export const verifyaccessToken=async(is_Admin:boolean)=>{
         const {success,tokendata}=await verifyAccessTokenS(accessToken);
         if(!success) return res.status(401).json({success:false,message:"invalid token credentials"})
        
-        
-        console.log("token verified now verify role",req.user)
-        if(!is_Admin==tokendata.is_Admin) return res.status(400).json({success:false,error:"authorization role not valid"})
         //store the token data req.user
         req.userData=tokendata
         next()  
@@ -28,10 +21,24 @@ export const verifyaccessToken=async(is_Admin:boolean)=>{
         console.log(e)
         return res.status(401).json({success:false,error:e.message})
     }
-    }
+    
         
 }
 
+//verify roles for speciific api end points
+export const verifyRole=async(is_Admin:boolean)=>{
+
+
+    return async(req:Request,res:Response,next:NextFunction)=>{
+        try{
+            console.log("token verified now verify role",req.user)
+        if(!is_Admin==req.userData.is_Admin) return res.status(400).json({success:false,error:"authorization role not valid"})
+        next()
+        }catch(e){
+            console.log(e)
+        }
+    }
+}
 
 
 
