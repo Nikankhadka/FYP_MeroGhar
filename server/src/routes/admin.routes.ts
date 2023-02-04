e3484406956e0e20334f50013d2a6c7ea4202cef
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-02-04 22:51:15 +0545

    same

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { getKycRequestsC, registerAdminC } from "../controllers/admin.controller";
+import { getKycRequestsC, registerAdminC, verifyKycRequestsC } from "../controllers/admin.controller";
 import { verifyaccessToken, verifyRole } from "../middlewares/auth.middleware";
 import { validateInput } from "../middlewares/inputvalidation";
 
@@ -12,7 +12,7 @@ router.use(verifyRole(true))
 
 router.post("/registerAdmin",validateInput,registerAdminC)
 router.get("/kycRequests",getKycRequestsC)
-router.patch("/kycRequests/:id")
+router.patch("/kycRequests/:id",verifyKycRequestsC)
 
 
 
