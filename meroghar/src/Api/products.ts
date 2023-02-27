076a322b6475516b80d6b1a93d075d9cd661430d
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-02-27 11:08:55 +0545

    remove property from the wishlist

---
+++ meroghar/src/Api/products.ts
@@ -0,0 +1,5 @@
+import 'server-only'
+
+//this way these api call only be called in the server components no in client component which is best next js practice 
+
+//then create seperate cleint componnets to call in server component/page where the data will be hydrated
--- server/src/controllers/property/wishlist.controller.ts
+++ server/src/controllers/property/wishlist.controller.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import { addPropertyWishS, getWishListS,getWishPropertyListS } from "../../services/property/wishlist.service";
+import { addPropertyWishS, getWishListS,getWishPropertyListS, removePropertyS } from "../../services/property/wishlist.service";
 
 
 export const addPropertyWishC=async(req:Request,res:Response)=>{
@@ -30,6 +30,17 @@ export const getWishPropertyListC=async(req:Request,res:Response)=>{
         if(wishList) return res.status(200).json({success:true,wishList})
     }catch(e:any){
         console.log(e);
+        res.status(400).json({success:false,error:e.message})
+    }
+}
+
+
+export const removeProperyC=async(req:Request,res:Response)=>{
+    try{
+        const propertyRemove=await removePropertyS(req.userData.userId,req.params.propertyId)
+        if(propertyRemove) return res.status(200).json({success:true,message:`Property ${req.params.propertyId} removed successfully from the wishlist`})
+    }catch(e:any){
+        console.log(e)
         res.status(400).json({success:false,error:e.message})
     }
 }
