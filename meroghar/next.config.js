09ef757547223c1797645bb198a94775cdece5ef
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-06-02 18:23:37 +0545

    update

--- meroghar/next.config.js
+++ meroghar/next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental:{
+    appDir:true,
+  },
+ 
+
+
+  // typescript: {
+    
+  //     // Dangerously allow production builds to successfully complete even if
+  //     // your project has type errors.
+  //     ignoreBuildErrors: true,
+  //  },
+
+  //  redirect handling for routes
+  //  async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/Home',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+  
+}
 
 module.exports = nextConfig
