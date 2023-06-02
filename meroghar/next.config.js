4c759a4b3a96169f4568cd4c2d3ed3232d188854
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-06-02 18:15:08 +0545

    update

--- meroghar/next.config.js
+++ meroghar/next.config.js
@@ -1,35 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental:{
-    appDir:true,
-  },
-  webpack(config){
-    config.module.rules.push({
-      test:/\.svg$/,
-      use:[{loader:'@svgr/webpack',options:{icon:true}}]
-    })
-    return config
-  },
-
-
-  // typescript: {
-    
-  //     // Dangerously allow production builds to successfully complete even if
-  //     // your project has type errors.
-  //     ignoreBuildErrors: true,
-  //  },
-
-  //  redirect handling for routes
-  //  async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/Home',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-  
-}
+const nextConfig = {}
 
 module.exports = nextConfig
