039ad69d1c731ba6c6189d3aa5ce67af38185586
Nikan Khadka <nikan.khadka.2002@gmail.com>
2023-06-05 10:53:47 +0545

    added helmet middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,16 +21,17 @@ import indexRouter from "./routes/index.routes"
 import { clearUser } from "./middlewares/auth.middleware"
 import dbConnect from "./configs/db"
 import cron from 'node-schedule'
-
+import helmet from 'helmet'
 
 
 //app level middleware setup
 dotenv.config() 
+app.use(helmet());
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({extended:true,limit:'10mb'}))
 app.use(cors({
     //defines the origin of the request
-    origin:"https://meroghar.vercel.app",
+    origin:["https://meroghar.vercel.app","https://meroghar-rf5q.onrender.com","http://localhost:3000"],
     //headers can be accessed and modified else cant
     credentials:true
 }))
@@ -50,6 +51,8 @@ app.use(session({
     //can store session data in db incase server crash the data is not lost
 //  store:
 }))
+
+
 
 //attaches cookie from header to req object
 app.use(cookieParser())
@@ -88,13 +91,17 @@ rule.minute=10;
 
 const scheduleJob=cron.scheduleJob(rule,async()=>{
     try{
-
+        console.log("cron job started api calling ")
         const res=fetch('https://meroghar-rf5q.onrender.com/property/v1/getProperty?limit=10&page=1',
         {
             method:"GET",
             credentials: 'include',
             cache:'no-store',
         }).then(res=>res.json()).catch(e=>console.log(e))
+
+        console.log("cron job end api called successfully")
+
+
 
     }catch(e){
         console.log(e);
